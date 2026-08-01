a1785c9cc124690cfb3cedef2146d0fceffcbed4
nehalneekhra <nehalneekhra13@gmail.com>
2026-08-01 19:15:40 +0530

    Setup Express backend and database configuration

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const connectDB = require("./config/db");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "🚀 FindIt Backend is Running Successfully!"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
